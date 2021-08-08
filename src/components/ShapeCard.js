42a7553d70cd9ceb8b1e44807a040dc8e57ba3db
slowgen <kalibobailey@gmail.com>
2021-08-08 12:03:26 -0400

    Feat: Edit shapes implemented, size and color

--- src/components/ShapeCard.js
+++ src/components/ShapeCard.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch} from 'react-redux';
-import {deleteShape} from '../store/shapes';
+import {deleteShape, editShape} from '../store/shapes';
 import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone'
 
 const ShapeCard = (props) => {
     const {shape} = props;
-
     const dispatch = useDispatch();
+
+    const [width, setWidth] = useState(Math.round(shape.width));
+    const [height, setHeight] = useState(Math.round(shape.height));
+    const [radius, setRadius] = useState(Math.round(shape.radius));
+    const [color, setColor] = useState(shape.color);
+
+    console.log('width', width)
 
     const handleClick = () => {
         dispatch(deleteShape(shape));
     }
 
-    // const handleChange = (e) => {}
+    const handleWidthChange = (e) => {
+        setWidth(Math.round(e.target.value));
+        dispatch(editShape(shape, 'width', width))
+    }
+
+    const handleHeightChange = (e) => {
+        setHeight(Math.round(e.target.value));
+        dispatch(editShape(shape, 'height', height))
+    }
+
+    const handleRadiusChange = (e) => {
+        setRadius(Math.round(e.target.value));
+        dispatch(editShape(shape, 'radius', radius))
+    }
+
+    const handleColorChange = (e) => {
+        setColor(e.target.value);
+        dispatch(editShape(shape, 'color', color))
+    }
 
     return(
         <div className="shape-card">
@@ -44,26 +68,47 @@ const ShapeCard = (props) => {
             </div>
             <div className="shape-card-row">
                 <div>
-                    {shape.type === 'rectangle' ? <div>Width</div> : <div>Radius</div>}
+                    {shape.type === 'rectangle' ? 
+                    (
+                        <label htmlFor="width">Width:</label>
+                    ) : ( 
+                        <label htmlFor="radius">Radius:</label>
+                    )}
                 </div>
                 <div>
-                    {shape.type === 'rectangle' ? Math.round(shape.width) : Math.round(shape.radius)}
+                    {shape.type === 'rectangle' ?  
+                        (
+                            <input type="range" id="width" name="width" 
+                            min={0} max={500} value={width} 
+                            onChange={handleWidthChange} />
+                        ) : (
+                            <input type="range" id="radius" name="radius"
+                            min={0} max={250} value={radius}
+                            onChange={handleRadiusChange} />
+                        )
+                }
                 </div>
             </div>
             <div className="shape-card-row">
                 <div>
-                    {shape.type === 'rectangle' && <div>Height</div>}
+                    {shape.type === 'rectangle' && <label htmlFor="height">Height:</label>}
                 </div>
                 <div>
-                    {shape.type === 'rectangle' && Math.round(shape.height)}
+                    {shape.type === 'rectangle' && 
+                        <input type="range" id="height" name="height"
+                        min={0} max={500} value={height}
+                        onChange={handleHeightChange} />
+                    }
                 </div>
             </div>
             <div className="shape-card-row">
                 <div>
-                    Color
+                    <label htmlFor="color">Color:</label>
                 </div>
                 <div>
-                    Colorpicker box
+                    <input type="color" id='color' name='color'
+                    value={color}
+                    onChange={handleColorChange} />
                 </div>
             </div>
         </div>
